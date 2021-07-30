37d98698155e14c8b33fbd3d968355dc255aadea
Davide De Rosa <keeshux@gmail.com>
2021-07-30 10:42:32 +0200

    Fix presets

--- scripts/net.rb
+++ scripts/net.rb
@@ -18,40 +18,45 @@ cfg = {
     ca: ca,
     client: client,
     key: key,
-    ep: [
-        "UDP:1194"
-    ],
     frame: 0,
     wrap: tls_wrap,
     eku: true
 }
 
-cfg_256 = cfg.dup
-cfg_256["cipher"] = "AES-256-GCM"
-cfg_256["auth"] = "SHA256"
-
 cfg_128 = cfg.dup
 cfg_128["cipher"] = "AES-128-CBC"
 cfg_128["auth"] = "SHA256"
+cfg_128["ep"] = [
+    "UDP:1196",
+    "TCP:445"
+]
 
-cfg_256 = {
-    id: "cfg256",
-    name: "256-bit",
-    comment: "256-bit encryption",
-    cfg: cfg_256
-}
+cfg_256 = cfg.dup
+cfg_256["cipher"] = "AES-256-GCM"
+cfg_256["auth"] = "SHA256"
+cfg_256["ep"] = [
+    "UDP:1194",
+    "TCP:443"
+]
+
 cfg_128 = {
     id: "cfg128",
     name: "128-bit",
     comment: "128-bit encryption",
     cfg: cfg_128
 }
-presets = [cfg_256, cfg_128]
+cfg_256 = {
+    id: "cfg256",
+    name: "256-bit",
+    comment: "256-bit encryption",
+    cfg: cfg_256
+}
+presets = [cfg_128, cfg_256]
 
 defaults = {
     :username => "myusername",
     :pool => "us",
-    :preset => "cfg256"
+    :preset => "cfg128"
 }
 
 ###
