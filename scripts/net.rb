01eb00b4e24d4830eeead9a416d07e56b10c2dc7
Davide De Rosa <keeshux@gmail.com>
2022-03-25 09:34:46 +0100

    Upgrade to v5

--- scripts/net.rb
+++ scripts/net.rb
@@ -8,95 +8,99 @@ load "util.rb"
 
 ###
 
-servers = File.read("../template/servers.json")
+template = File.read("../template/servers.json")
 ca = File.read("../static/ca.crt")
 client = File.read("../static/client.crt")
 key = File.read("../static/client.key")
 tls_wrap = read_tls_wrap("auth", 1, "../static/ta.key", 1)
 
 cfg = {
-    ca: ca,
-    client: client,
-    key: key,
-    frame: 0,
-    wrap: tls_wrap,
-    eku: true
+  ca: ca,
+  clientCertificatea: client,
+  clientKey: key,
+  compressionFraming: 0,
+  tlsWrap: tls_wrap,
+  checksEKU: true
 }
 
 cfg_128 = cfg.dup
 cfg_128["cipher"] = "AES-128-CBC"
 cfg_128["auth"] = "SHA256"
-cfg_128["ep"] = [
-    "UDP:1196",
-    "TCP:445"
-]
 
 cfg_256 = cfg.dup
 cfg_256["cipher"] = "AES-256-GCM"
 cfg_256["auth"] = "SHA256"
-cfg_256["ep"] = [
-    "UDP:1194",
-    "TCP:443"
-]
 
 cfg_128 = {
-    id: "cfg128",
-    name: "128-bit",
-    comment: "128-bit encryption",
-    cfg: cfg_128
+  id: "cfg128",
+  name: "128-bit",
+  comment: "128-bit encryption",
+  ovpn: {
+    cfg: cfg_128,
+    endpoints: [
+      "UDP:1196",
+      "TCP:445"
+    ]
+  }
 }
 cfg_256 = {
-    id: "cfg256",
-    name: "256-bit",
-    comment: "256-bit encryption",
-    cfg: cfg_256
+  id: "cfg256",
+  name: "256-bit",
+  comment: "256-bit encryption",
+  ovpn: {
+    cfg: cfg_256,
+    endpoints: [
+      "UDP:1194",
+      "TCP:443"
+    ]
+  }
 }
 presets = [cfg_128, cfg_256]
 
 defaults = {
-    :username => "myusername",
-    :pool => "us",
-    :preset => "cfg128"
+  :username => "myusername",
+  :country => "US"
 }
 
 ###
 
-pools = []
+servers = []
 
-json = JSON.parse(servers)
+json = JSON.parse(template)
 json["countries"].each { |country|
-    country["cities"].each { |city|
-        code = country["code"].upcase
-        area = city["name"]
+  country["cities"].each { |city|
+    code = country["code"].upcase
+    area = city["name"]
 
-        city["relays"].each { |relay|
-            id = relay["hostname"]
-            hostname = "#{id}.oeck.com"
-            num = id.split("-").last.to_i
+    city["relays"].each { |relay|
+      id = relay["hostname"]
+      hostname = "#{id}.oeck.com"
+      num = id.split("-").last.to_i
 
-            addresses = [relay["ipv4_addr_in"]]
-            addresses.map! { |a|
-                IPAddr.new(a).to_i
-            }
+      addresses = [relay["ipv4_addr_in"]]
+      addresses.map! { |a|
+        IPAddr.new(a).to_i
+      }
 
-            pool = {
-                :id => id,
-                :country => code,
-                :addrs => addresses
-            }
-            pool[:area] = area if !area.empty?
-            pool[:num] = num
-            pools << pool
-        }
+      server = {
+        :id => id,
+        :country => code,
+        :hostname => hostname,
+        :addrs => addresses
+      }
+      server[:area] = area if !area.empty?
+      server[:num] = num
+      servers << server
     }
+  }
 }
 
 ###
 
 infra = {
-    :pools => pools,
-    :presets => presets,
-    :defaults => defaults
+  :servers => servers,
+  :presets => presets,
+  :defaults => defaults
 }
 
 puts infra.to_json
